71659a52f0eacbf5f0b2e389824779ed2808e567
utsavt9936 <ut13@iitbbs.ac.in>
2020-06-01 18:48:03 +0530

    share link done -2

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ global.myid
 
 pool.connect((err,client,done)=>{
 
-   console.log('clt',client)
+  // console.log('clt',client)
 
 
    globalThis.client=client
@@ -641,12 +641,10 @@ app.get('/:id/get_groups',authorizeParams,(req,res)=>{
     }
 })
 
-app.get('/share_link',authorizeParams,(req,res)=>{
+app.get('/:id/share_link',authorizeParams,(req,res)=>{
 
     //res.send("hello")
     var link="https://www.uvwave-invitation.com/visit/"+req.query.type+"/"+req.query.id+"/"+req.query.name;
-
-
 
     res.send({
 link
